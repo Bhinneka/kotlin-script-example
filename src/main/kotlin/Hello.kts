fbc60ef7a1d6edd5d98a6458bf1c862619a4057a
Budi Oktaviyan <budioktaviyans@gmail.com>
2019-02-13 13:55:38 +0700

    Initial sample for Kotlin compiler and Native compiler

--- src/main/kotlin/Hello.kts
+++ src/main/kotlin/Hello.kts
@@ -1,1 +1,13 @@
-println("Hello")
+import java.util.Calendar
+
+println("Hello\n")
+
+data class Company(val name: String, val year: Long)
+
+val myCompany = Company(name = "Bhinneka", year = 1993)
+for (i in 1 until 10) {
+    println("I love ${myCompany.name} since ${myCompany.year}")
+}
+
+val today = Calendar.getInstance().time
+println("\nToday is $today")
